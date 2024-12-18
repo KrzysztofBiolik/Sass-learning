c3ac41c5cd2b3ddd7e04c23343e3090b52e3c075
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-12-18 14:35:41 +0100

    Added Navigation styling

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,8 @@
 const menuBtn = document.querySelector(".menu-btn");
 const hamburger = document.querySelector(".menu-btn__burger");
+const nav = document.querySelector(".nav");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".menu-nav__item");
 
 let showMenu = false;
 
@@ -7,4 +10,7 @@ menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
   hamburger.classList.toggle("open");
+  nav.classList.toggle("open");
+  menuNav.classList.toggle("open");
+  navItems.forEach((item) => item.classList.toggle("open"));
 }
