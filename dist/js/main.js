b70eb8eda3ee85fa2e8185dc6195900984d14b88
Krzysztof Biolik <krzysztofbiolik01@gmail.com>
2024-12-23 23:52:58 +0100

    Added a function that changes the background and content on the page depending on the selected photo from the carousel container

--- dist/js/main.js
+++ dist/js/main.js
@@ -15,19 +15,40 @@ function toggleMenu() {
   navItems.forEach((item) => item.classList.toggle("open"));
 }
 
-function changeBg(bg, title) {
+async function changeBg(bg, title) {
   const banner = document.querySelector(".banner");
-  const contents = document.querySelectorAll(".banner__content");
+  const contents = document.querySelector(".banner__content");
+
+  const repo = await axios.get(
+    `https://api.github.com/repos/krzysztofbiolik/${title}`
+  );
+  const repoData = await repo.data;
+  console.log(repoData);
+
   banner.style.background = `url("./img/${bg}")`;
-  banner.style.backgrounSize = "cover";
+  banner.style.backgroundSize = "cover";
   banner.style.backgroundPosition = "center";
 
-  contents.forEach((content) => {
-    content.classList.remove("active");
-    if (content.classList.contains(title)) {
-      content.classList.add("active");
-    }
-  });
+  contents.innerHTML = `  
+    <h2>${repoData.name}</h2>
+    <p>${repoData.description}</p>
+    <div class="button">
+      <a 
+      target="_blank"
+      rel="noreferrer"
+      href="${repoData.homepage}">
+        <i class="fa fa-play" aria-hidden="true"></i>
+          Demo
+      </a>
+      <a 
+      target="_blank"
+      rel="noreferrer"
+      href="${repoData.html_url}">
+        <i class="fa fa-plus" aria-hidden="true"></i>
+          Code
+      </a>  
+    </div>    
+`;
 }
 
 const getGithubRepos = async () => {
@@ -48,7 +69,7 @@ const displayRepos = async () => {
   for (const result of reposData) {
     console.log(
       (htmlString += `
-        <div class="carousel-item" onclick="changeBg('${result.imageName}.jpg', '${name}')">
+        <div class="carousel-item" onclick="changeBg('bg-${result.name}.jpg', '${result.name}')">
           <img src="./img/${result.imageName}.jpg" alt="${result.name}" />
         </div>
       `)
@@ -58,13 +79,6 @@ const displayRepos = async () => {
   document.querySelector(".carousel").innerHTML = htmlString;
 };
 
-
-
 displayRepos();
 
-const getGithubRepo = () => {
-  fetch("https://api.github.com/repos/krzysztofbiolik/Todos-list-react")
-    .then((res) => res.json())
-    .then((data) => data[8])
-    .then((result) => console.log(result.description));
-};
+
